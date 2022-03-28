d6499db18ffabc5ee633dd70a975a5550eaf908b
Jadoicinic1 <iconicmustapha@gmail.com>
2022-03-28 02:19:09 +0200

    Sign In Form

--- src/components/Screen/Auth.js
+++ src/components/Screen/Auth.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div className='form-group'>
+        <input className='form-control' placeholder='Username' />
+        <input type='password' className='form-control' placeholder='Password' />
+        <button className='btn btn-success form-control'>Sign In</button>
+      </div>
+    </div>
   )
 }
 
