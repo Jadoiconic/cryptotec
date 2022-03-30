2ef336d3b7f435cb8899ba298241b787b2605656
Jadoicinic1 <iconicmustapha@gmail.com>
2022-03-30 12:05:17 +0200

    on 30th march

--- src/components/Screen/NoPage.js
+++ src/components/Screen/NoPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NoPage() {
   return (
     <div className='container-fluid'>
-        <h1 className='text-primary bg-secondary'> Page Not found</h1>
+        <h1 className='text-warning bg-primary h-100 w-100'> Page Not found</h1>
     </div>
   )
 }
